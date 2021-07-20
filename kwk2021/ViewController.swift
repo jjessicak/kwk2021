d96b96d80d751f67411a901ea25d15e143099dbb
Jessica <jessicajkim13@gmail.com>
2021-07-20 12:06:43 -0700

    Initial Commit

---
+++ kwk2021/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kwk2021
+//
+//  Created by John Kim on 2021-07-20.
+//  Copyright © 2021 Jessica Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ kwk2021Tests/kwk2021Tests.swift
@@ -0,0 +1,34 @@
+//
+//  kwk2021Tests.swift
+//  kwk2021Tests
+//
+//  Created by John Kim on 2021-07-20.
+//  Copyright © 2021 Jessica Kim. All rights reserved.
+//
+
+import XCTest
+@testable import kwk2021
+
+class kwk2021Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
